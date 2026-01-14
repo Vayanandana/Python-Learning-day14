f3a6aa0ca4051cf1c9f0a7f21b3e6194723add08
VayanandanaBuddavarapu <vayanandanabud@gmail.com>
2026-01-14 08:01:00 +0530

    Add files via upload

---
+++ relationaloperators.py
@@ -0,0 +1,24 @@
+'''
+Relational operators in Python are used to compare values and return a Boolean result (True or False).
+They play a crucial role in conditional statements, loops, and decision-making logic.
+'''
+a = 10
+b = 20
+
+print(a == b)   # False
+print(a != b)   # True
+print(a > b)    # False
+print(a < b)    # True
+print(a >= b)   # False
+print(a <= b)   # True
+
+#With User Input
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
